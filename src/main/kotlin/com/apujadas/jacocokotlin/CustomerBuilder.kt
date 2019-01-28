594cec0ad663952ebd43399c2ada90209a3896bf
apujadas <andres.pujadas@mercadolibre.com>
2019-01-28 09:40:07 -0300

    private setters

--- src/main/kotlin/com/apujadas/jacocokotlin/CustomerBuilder.kt
+++ src/main/kotlin/com/apujadas/jacocokotlin/CustomerBuilder.kt
@@ -3,7 +3,9 @@ package com.apujadas.jacocokotlin
 class CustomerBuilder {
 
     var id: Long? = null
+        private set
     var name: String? = null
+        private set
 
     fun id(id: Long) = apply { this.id = id }
     fun name(name: String) = apply { this.name = name }
